7141946192b8e5b367ffc4a4cccde81e77e8ba29
ligas-pratama-yoga <ligas7231@gmail.com>
2025-03-22 22:05:33 +0700

    Commit Kedelapan:Mengubah partials/navigation.php

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -4,23 +4,23 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <div class="sb-sidenav-menu-heading">Core</div>
-                            <a class="nav-link" href="ukom-rpl/public/">
+                                <a class="nav-link" href="<?= $base_url ?>/">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
-                            <a class="nav-link" href="ukom-rpl/public/produk">
+<a class="nav-link" href="<?= $base_url ?>/produk">
                                 <div class="sb-nav-link-icon"><i class="fa-solid fa-box"></i></div>
                                 Produk
                             </a>
-                            <a class="nav-link" href="ukom-rpl/public/transaksi">
+<a class="nav-link" href="<?= $base_url ?>/transaksi">
                                 <div class="sb-nav-link-icon"><i class="fa-solid fa-money-bill"></i></div>
                                 Transaksi
                             </a>
-                            <a class="nav-link" href="ukom-rpl/public/users">
+<a class="nav-link" href="<?= $base_url ?>/users">
                                 <div class="sb-nav-link-icon"><i class="fa-solid fa-users"></i></div>
                                 Users
                             </a>
-                            <a class="nav-link" href="ukom-rpl/public/login">
+<a class="nav-link" href="<?= $base_url ?>/login">
                                 <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
                                 Login
                             </a>
