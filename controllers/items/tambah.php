0c55e1366370fcec7f644c814655819bd4949f7d
ligas-pratama-yoga <ligas7231@gmail.com>
2025-04-10 20:06:19 +0700

    Commit

--- controllers/items/tambah.php
+++ controllers/items/tambah.php
@@ -6,7 +6,9 @@ use Models\Produk;
 require __DIR__ . "/../../models/Items.php";
 require __DIR__ . "/../../models/Produk.php";
 
-Items::create($_POST);
+if (!Items::create($_POST)) {
+    header("location: $base_url/transaksi/{$_POST['id_transaksi']}");
+}
 $stok = Produk::find($_POST['id_produk'])["stok"];
 Produk::update(["stok" => $stok - $_POST['subtotal']])
     ->where('id', $_POST['id_produk']);
